4f4efdadc981db24da0bb374fed2375ae56c5beb
Kalle Kromann <kallekro@gmail.com>
2019-04-22 21:10:54 +0200

    saving to file and speed adjustments

--- gameoflife.py
+++ gameoflife.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import filedialog
+from tkinter.messagebox import showerror
 from time import sleep, time
 import threading
 import math
@@ -6,9 +8,10 @@ import queue
 from collections import deque
 
 CANVAS_SIZE = (1000, 800)
+UPDATE_FREQS = [0.5, 0.25, 0.125, 0.06, 0.03]
 MAX_CELLSIZE = 100
-UPDATE_FREQ = 0.5
 ZOOM_FREQ = 0.5
+UNDO_HISTORY_LENGTH = 1000
 
 class GameState():
     def __init__(self):
@@ -63,7 +66,6 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.grid(padx=5, pady=5)
-        self.createWidgets()
 
         self.shutdown = False
         self.inputQueue = queue.Queue()
@@ -75,12 +77,16 @@ class Application(tk.Frame):
         self.undoStates = deque()
 
         self.viewOffset = (0, 0)
+
+        self.update_freq_idx = 0
         self.updatelock = threading.Lock()
         self.updateWorker = threading.Thread(target=self.updateLoop)
         self.lastUpdate = time()
         self.lastZoom = 0
-        self.drawGrid()
+
         self.master.protocol("WM_DELETE_WINDOW", self.quitApp)
+        self.createWidgets()
+        self.drawGrid()
         self.updateWorker.start()
 
     def quitApp(self):
@@ -92,7 +98,7 @@ class Application(tk.Frame):
 
     ### Update ###
     def updateStep(self):
-        if len(self.undoStates) >= 100:
+        if len(self.undoStates) >= UNDO_HISTORY_LENGTH:
             self.undoStates.popleft()
         self.undoStates.append(SavedState(self.gamestate.cells))
 
@@ -102,10 +108,10 @@ class Application(tk.Frame):
 
     def popQueuedInput(self, block):
         f, args = self.inputQueue.get(block)
-        if len(args) == 2:
-            f(self, args[1]) # event input
-        else:
-            f(self) # button press
+        if len(args) == 2: # event input
+            f(self, args[1])
+        else: # button press
+            f(self)
         self.inputQueue.task_done()
 
     def updateLoop(self):
@@ -117,7 +123,7 @@ class Application(tk.Frame):
                     return
                 if self.gamestate.running:
                     now = time()
-                    if now - self.lastUpdate > UPDATE_FREQ:
+                    if now - self.lastUpdate >= UPDATE_FREQS[self.update_freq_idx]:
                         self.lastUpdate = now
                         self.updateStep()
             try:
@@ -185,7 +191,6 @@ class Application(tk.Frame):
             return
         self.refreshView(drawGrid=False)
 
-    ### State control ###
     @queueInput
     def toggleGameUpdates(self):
             self.gamestate.running = not self.gamestate.running
@@ -193,6 +198,41 @@ class Application(tk.Frame):
     @queueInput
     def saveState(self):
         self.lastSavedState = SavedState(self.gamestate.cells)
+
+    @queueInput
+    def saveStateToFile(self):
+        filename = filedialog.asksaveasfilename(initialdir=__file__, title="Select state file", filetypes=(("text", "*.txt"), ("all files", "*.*")))
+        if filename:
+            with open(filename, 'w') as fd:
+                for cell in self.gamestate.cells:
+                    fd.write(f"{cell[0]},{cell[1]}\n")
+
+    @queueInput
+    def loadStateFromFile(self):
+        filename = filedialog.askopenfilename(initialdir=__file__, title="Select state file", filetypes=(("text", "*.txt"), ("all files", "*.*")))
+        if filename:
+            linecount = 0
+            newcells = []
+            lowestCoords = None
+            with open(filename, 'r') as fd:
+                try:
+                    for line in fd:
+                        linecount += 1
+                        cell_parts = line.split(',')
+                        if len(cell_parts) != 2:
+                            raise ValueError
+                        cell = (int(cell_parts[0]), int(cell_parts[1]))
+                        if lowestCoords:
+                            lowestCoords = (min(lowestCoords[0], cell[0]), min(lowestCoords[1], cell[1]))
+                        else:
+                            lowestCoords = cell
+                        newcells.append(cell)
+                except ValueError:
+                    showerror(title="Load error", message="Error loading state save file on line %d" % linecount)
+                    return
+            self.viewOffset = (-lowestCoords[0], -lowestCoords[1])
+            self.gamestate.cells = newcells
+            self.refreshView()
 
     @queueInput
     def loadState(self):
@@ -215,30 +255,57 @@ class Application(tk.Frame):
             self.gamestate.cells = self.undoStates.pop().cells
             self.refreshView()
 
+    def updateSpeedLabel(self):
+        self.speedStringVar.set(f"Speed: x{self.update_freq_idx + 1}")
+
+    @queueInput
+    def increaseSpeed(self):
+        if self.update_freq_idx >= len(UPDATE_FREQS)-1: return
+        self.update_freq_idx += 1
+        self.updateSpeedLabel()
+
+    @queueInput
+    def decreaseSpeed(self):
+        if self.update_freq_idx <= 0: return
+        self.update_freq_idx -= 1
+        self.updateSpeedLabel()
+
     ### Init ###
     def createWidgets(self):
-        self.quitButton = tk.Button(self, text='Quit', command=self.quitApp)
-        self.quitButton.grid(row=0, column=0, sticky="W")
+        self.quitButton = tk.Button(self, text='Quit', command=self.quitApp, width=10)
+        self.quitButton.grid(row=0, column=0, sticky="NW")
 
         self.gameControlFrame = tk.Frame(self)
-        self.gameControlFrame.grid(row=0, column=1, sticky="E")
-        self.startButton = tk.Button(self.gameControlFrame, text='Start/Pause', command=self.toggleGameUpdates)
+        self.gameControlFrame.grid(row=0, column=1, sticky="NE")
+        self.startButton = tk.Button(self.gameControlFrame, text='Start/Pause', command=self.toggleGameUpdates, width=10)
         self.startButton.grid(row=0, column=0)
-        self.stepButton = tk.Button(self.gameControlFrame, text='Step', command=self.manualStep)
+        self.stepButton = tk.Button(self.gameControlFrame, text='Step', command=self.manualStep, width=10)
         self.stepButton.grid(row=0, column=1)
-        self.stepButton = tk.Button(self.gameControlFrame, text='Step back', command=self.manualStepBack)
+        self.stepButton = tk.Button(self.gameControlFrame, text='Step back', command=self.manualStepBack, width=10)
         self.stepButton.grid(row=0, column=2)
+        self.speedStringVar = tk.StringVar()
+        self.updateSpeedLabel()
+        self.speedLabel = tk.Label(self.gameControlFrame, textvariable=self.speedStringVar)
+        self.speedLabel.grid(row=1, column=0)
+        self.speedIncreaseButton = tk.Button(self.gameControlFrame, text='Faster', command=self.increaseSpeed, width=10)
+        self.speedIncreaseButton.grid(row=1, column=1)
+        self.speedDecreaseButton = tk.Button(self.gameControlFrame, text='Slower', command=self.decreaseSpeed, width=10)
+        self.speedDecreaseButton.grid(row=1, column=2)
 
         self.stateControlFrame = tk.Frame(self)
         self.stateControlFrame.grid(row=0, column=2, sticky="E")
-        self.saveButton = tk.Button(self.stateControlFrame, text='Save state', command=self.saveState)
+        self.saveButton = tk.Button(self.stateControlFrame, text='Quicksave', command=self.saveState, width=10)
         self.saveButton.grid(row=0, column=0)
-        self.loadButton = tk.Button(self.stateControlFrame, text='Reload state', command=self.loadState)
+        self.loadButton = tk.Button(self.stateControlFrame, text='Load quicksave', command=self.loadState, width=10)
         self.loadButton.grid(row=0, column=1)
-        self.loadButton = tk.Button(self.stateControlFrame, text='Clear state', command=self.clearState)
+        self.loadButton = tk.Button(self.stateControlFrame, text='Clear state', command=self.clearState, width=10)
         self.loadButton.grid(row=0, column=2)
+        self.saveToFileButton = tk.Button(self.stateControlFrame, text='Save to file', command=self.saveStateToFile, width=10)
+        self.saveToFileButton.grid(row=1, column=0)
+        self.saveToFileButton = tk.Button(self.stateControlFrame, text='Load from file', command=self.loadStateFromFile, width=10)
+        self.saveToFileButton.grid(row=1, column=1)
 
-        self.canvas = tk.Canvas(self, width=CANVAS_SIZE[0], height=CANVAS_SIZE[1], bg='white', bd=2, relief="groove")
+        self.canvas = tk.Canvas(self, width=CANVAS_SIZE[0], height=CANVAS_SIZE[1], bg='black', bd=2, relief="groove")
         self.canvas.bind("<Button-1>", self.leftClickedCanvasCallback)
         self.canvas.bind("<Button-3>", self.rightClickedCanvasCallback)
         self.canvas.bind("<B1-Motion>", self.leftClickedCanvasCallback)
@@ -263,7 +330,7 @@ class Application(tk.Frame):
     def addCell(self, cell):
         x = (cell[0] + self.viewOffset[0])*self.cellsize
         y = (cell[1] + self.viewOffset[1])*self.cellsize
-        self.cell_rectangles[cell] = self.canvas.create_rectangle(x, y, x + self.cellsize, y + self.cellsize, fill="blue")
+        self.cell_rectangles[cell] = self.canvas.create_rectangle(x, y, x + self.cellsize, y + self.cellsize, fill="OrangeRed2", outline="gray22")
 
     def interfaceAddCell(self, cell):
         if cell not in self.gamestate.cells:
@@ -294,11 +361,11 @@ class Application(tk.Frame):
         w = self.canvas.winfo_width()
         h = self.canvas.winfo_height()
         while i < w:
-            self.canvas.create_line(i, 0, i, h)
+            self.canvas.create_line(i, 0, i, h, fill="gray22")
             i += self.cellsize
         i = 0
         while i < h:
-            self.canvas.create_line(0, i, w, i)
+            self.canvas.create_line(0, i, w, i, fill="gray22")
             i += self.cellsize
 
 if __name__ == "__main__":
